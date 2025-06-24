933d8bf431e11ec64fc3390893aca0e717a754bb
Maham Mansoor <mansoormaham26@gmail.com>
2025-06-24 13:54:52 +0500

    Create task1.py
     User input handling
    	•	 Typecasting using int() and float()
    	•	 Formatted strings using f-strings
    	•	 Basic data types (int, float, str)
    	•	 type() usage for extra learning (shows data type name)

---
+++ task1.py
@@ -0,0 +1,16 @@
+#taking inputs from user
+#integer
+age= int (input("Enter your age: "))
+#float
+weight = float(input("Enter your weight: "))
+#string
+name = input("Enter a string: ")
+
+
+# Displaying values using formatted print statements
+print(" Here are inputs:")
+print(f" Integer value: {age} (Type: {type(age)})")
+print(f" Float value: {weight} (Type: {type(weight)})")
+print(f"String value: '{name}' (Type: {type(name)})")
+
+
